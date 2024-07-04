2f85a8ef21e7aae2fa7534c8ece578842a9e132b
Jellooking <jassimkhalaf14@gmail.com>
2024-07-05 01:19:35 +0300

    adjusted and added more files to MyItems folder :D

---
+++ swan-shine/src/componenets/MyItems/AddItemButton.jsx
@@ -0,0 +1,9 @@
+//we can just import a cool looking button here :D
+import React from 'react'
+import './AddItemButton.css'
+
+const AddItemButton = () => {
+  return <button className="add-item-button">Add Item</button>
+}
+
+export default AddItemButton
