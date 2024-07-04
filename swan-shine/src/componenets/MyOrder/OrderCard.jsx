7e8bd6b6ce7cda2ad8affa81c0d2015c92fecdb5
Jellooking <jassimkhalaf14@gmail.com>
2024-07-05 01:31:14 +0300

    created files for the order and receipt

---
+++ swan-shine/src/componenets/MyOrder/OrderCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './OrderCard.css'
+
+const OrderCard = () => {
+  return (
+    <div className="order-card">
+      <h2>Order</h2>
+      <p>Details about the order go here.</p>
+    </div>
+  )
+}
+
+export default OrderCard
