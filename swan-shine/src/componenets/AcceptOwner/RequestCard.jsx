a6dfe7fb810892fc3009e3f399a9aae852b0fb58
Jellooking <jassimkhalaf14@gmail.com>
2024-07-05 00:50:03 +0300

    base Accept Owner page done

---
+++ swan-shine/src/componenets/AcceptOwner/RequestCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './RequestCard.css'
+
+const RequestCard = () => {
+  return (
+    <div className="request-card">
+      <h2>Owner Request</h2>
+      <p>Details of the owner's request go here.</p>
+    </div>
+  )
+}
+
+export default RequestCard
