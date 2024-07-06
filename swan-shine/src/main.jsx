56d20d4230802989b4de6c4d55286407cc5edbec
jenan <jenanmj15@gmail.com>
2024-07-06 17:46:23 +0300

    to mearge

--- swan-shine/src/main.jsx
+++ swan-shine/src/main.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
-
+// to merge
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
