7a450736bf849a7f8e263c17f18af61bed3a5331
jenan <jenanmj15@gmail.com>
2024-07-07 13:17:37 +0300

    instill axios

--- swan-shine/src/components/header/header.jsx
+++ swan-shine/src/components/header/header.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './App.css'
-// to merge
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
-heder
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 import React from 'react'
 import { Link } from 'react-router-dom'
