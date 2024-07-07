3c3c45f6f86bdc2aadca23ca9865496d3f70a922
jenan <jenanmj15@gmail.com>
2024-07-07 13:35:07 +0300

    fish the items detalis card

--- swan-shine/src/components/ItemDetails/ItemDetailsPage.jsx
+++ swan-shine/src/components/ItemDetails/ItemDetailsPage.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import Review from './Review'
-import Rating from './Rating'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import ItemDetailsCard from './ItemDetailsCard'
 import './ItemDetailsPage.css'
 
 const ItemDetailsPage = () => {
+  const [item, setItem] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const itemId = '1'
+
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/items/show/${itemId}`
+        )
+        setItem(response.data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching the item details:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchItemDetails()
+  }, [itemId])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!item) {
+    return <div>Error loading item details</div>
+  }
+
   return (
     <div className="item-details-page">
       <h1>Item Details</h1>
-      <ItemDetailsCard />
-      <Review />
-      <Rating />
+      <ItemDetailsCard item={item} />
     </div>
   )
 }
