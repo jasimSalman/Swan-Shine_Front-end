e7bb4619d6d6f9c508b64c837dfe133078673cdd
jenan <jenanmj15@gmail.com>
2024-07-07 13:54:19 +0300

    propss data to items detalis ps ge

--- swan-shine/src/components/ItemDetails/ItemDetailsCard.jsx
+++ swan-shine/src/components/ItemDetails/ItemDetailsCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './ItemDetailsCard.css'
 
 const ItemDetailsCard = ({ item }) => {
   return (
     <div className="item-details-card">
       <img src={item.image} alt={item.name} className="item-image" />
       <h2>{item.name}</h2>
-      <p>{item.reviews.length} Reviews</p>
+      <p>Category: {item.category.name}</p>
       <p>Price: ${item.price}</p>
       <button>Add to Cart</button>
     </div>
