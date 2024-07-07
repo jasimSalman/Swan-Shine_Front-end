52ff3d90166a25f26f886435683e839b4767e996
jenan <jenanmj15@gmail.com>
2024-07-07 15:32:08 +0300

    create header with react bootstrap

--- swan-shine/src/components/header/header.jsx
+++ swan-shine/src/components/header/header.jsx
@@ -1,48 +1,36 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import NavScrollExample from './components/NavBar'
+import HomePage from '../Home page/homepage'
+import CategoryPage from '../Category/CategoryPage'
+import MyItemsPage from '../MyItems/MyItemsPage'
+import FavoriteListPage from '../FavoriteList/FavoriteListPage'
+import CartPage from '../Cart/CartPage'
+import RegistrationForm from '../Login/RegistrationForm'
+import LoginForm from '../Login/LoginForm'
+
 import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const App = () => {
+  const user = null
+  const handleLogOut = () => {}
 
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../../public/logo.jpeg'
-import '../../src/App.css'
-
-const Header = ({ user, handleLogOut }) => {
   return (
-    <header>
-      <nav>
-        <div className="head">
-          <Link to="/">
-            <img src={logo} alt="Logo" className="logo" />
-          </Link>
-          <Link to="/">Home</Link>
-          <Link to="/categories">Categories</Link>
-          <Link to="/My_items">My Items</Link>
-          <Link to="/fav_basket">Favorites basket</Link>
-          <Link to="/basket">basket</Link>
-
-          {user ? (
-            <Link onClick={handleLogOut} to="/">
-              Sign Out
-            </Link>
-          ) : (
-            <>
-              <Link to="/register">Register</Link>
-              <Link to="/signin">Sign In</Link>
-            </>
-          )}
-        </div>
-        {user && (
-          <div>
-            <h3 className="greeting">Welcome {user.email}!</h3>
-          </div>
-        )}
-      </nav>
-    </header>
+    <Router>
+      <div>
+        <NavScrollExample user={user} handleLogOut={handleLogOut} />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/CategoryPage" element={<CategoryPage />} />
+          <Route path="/MyItemsPage" element={<MyItemsPage />} />
+          <Route path="/FavoriteListPage" element={<FavoriteListPage />} />
+          <Route path="/basCartPageket" element={<CartPage />} />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/signin" element={<LoginForm />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
-export default Header
+export default App
