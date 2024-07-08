2e0d0ba2659c7402af1b7d8209aa9c7a622a5c53
jenan <jenanmj15@gmail.com>
2024-07-08 17:15:20 +0300

    show data in catregory page

--- swan-shine/src/components/Category/CategoryPage.jsx
+++ swan-shine/src/components/Category/CategoryPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import ItemsCard from './ItemsCard'
+import React, { useEffect, useState } from 'react'
+import ItemsCard from '../Category/ItemsCard'
 import './CategoryPage.css'
+import axios from 'axios'
+import { BASE_URL } from '../../services/api'
 
 const CategoryPage = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/category`)
+      setCategories(response.data)
+    } catch (error) {
+      console.error('Error fetching categories:', error)
+    }
+  }
+
   return (
     <div className="category-page">
-      <h1>Category</h1>
-      <ItemsCard />
+      <h1>Categories</h1>
+      <div className="categories-grid">
+        {categories.map((category) => (
+          <div key={category._id} className="category-card">
+            <img src={category.poster} alt={category.name} />
+            <div className="category-details">
+              <h2>{category.name}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
