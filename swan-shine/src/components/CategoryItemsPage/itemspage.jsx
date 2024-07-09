9c2a19417a28c332bafb6901d6ad630129fd0190
jasimSalman <jassimmo7amed+1@gmail.com>
2024-07-09 21:31:59 +0300

    edit some files

---
+++ swan-shine/src/components/CategoryItemsPage/itemspage.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import ItemsCard from '../Shared/ItemsCard'
+import { BASE_URL } from '../../services/api'
+
+const ItemsPage = () => {
+  const { id } = useParams()
+  const [items, setItems] = useState([])
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/items/${id}`)
+      setItems(response.data)
+    } catch (error) {
+      console.error(`Error fetching items for category ${id}:`, error)
+    }
+  }
+
+  useEffect(() => {
+    fetchItems()
+  }, [id])
+
+  return (
+    <div className="items-page">
+      {items.length > 0 ? (
+        <ItemsCard items={items} />
+      ) : (
+        <p>No items found for this category.</p>
+      )}
+    </div>
+  )
+}
+
+export default ItemsPage
--- swan-shine/src/components/Shared/ItemsCard.jsx
+++ swan-shine/src/components/Shared/ItemsCard.jsx
@@ -1,43 +1,31 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import '../Cart/CartPage.css'
+import { useNavigate } from 'react-router-dom'
+import '../Shared/ItemsCard.css'
 
 const ItemsCard = ({ items }) => {
+  const navigate = useNavigate()
+
+  const handleCardClick = (id) => {
+    navigate(`/item-details/${id}`)
+  }
+
   return (
     <div className="items-card">
       {items.map((item) => (
-        <div key={item._id} className="item-card">
-          <img src={item.image} alt={item.name} />
+        <div
+          key={item._id}
+          className="item-card"
+          onClick={() => handleCardClick(item._id)}
+        >
+          <img src={item.image} alt={item.title} />
           <div className="item-details">
-            <div className="item-header">
-              <h2>{item.name}</h2>
-            </div>
-            <div className="item-footer">
-              <p>Price: ${item.price.toFixed(2)}</p>
-            </div>
+            <h2>{item.name}</h2>
+            <p>BD{item.price}</p>
           </div>
         </div>
       ))}
     </div>
   )
 }
-
-ItemsCard.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      price: PropTypes.number.isRequired,
-      quantity: PropTypes.number.isRequired
-    })
-  ).isRequired,
-  onRemoveItem: PropTypes.func.isRequired,
-  onUpdateQuantity: PropTypes.func.isRequired
-}
-
-// ItemsCard.defaultProps = {
-//   items: []
-// }
 
 export default ItemsCard
