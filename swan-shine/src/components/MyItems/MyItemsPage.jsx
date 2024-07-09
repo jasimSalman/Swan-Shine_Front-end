4a2a134e8ffc750ff76f04d602ba5d0b4ece9dcc
jasimSalman <jassimmo7amed+1@gmail.com>
2024-07-09 17:18:04 +0300

    show data on my items page

--- swan-shine/src/components/MyItems/MyItemsPage.jsx
+++ swan-shine/src/components/MyItems/MyItemsPage.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AddItemButton from './AddItemButton'
-import ItemsCard from './ItemsCard'
+import ItemsCard from '../Shared/ItemsCard'
 import Client from '../../services/api'
 // import './MyItemsPage.css'
 
 const MyItemsPage = () => {
-  const getItems = (async) => {
-    const res = Client.get(`user/shop/${shopId}/items`)
+  const userId = localStorage.getItem('userId')
+
+  const [items, setItems] = useState([])
+
+  const getItems = async () => {
+    const res = await Client.get(`users/shop/${userId}/items`)
+    setItems(res.data)
+    console.log(res.data)
   }
+
+  useEffect(() => {
+    getItems()
+  }, [userId])
 
   return (
     <div className="my-items-page">
       <h1>My Items</h1>
       <AddItemButton />
-      <ItemsCard />
+      <ItemsCard items={items} />
     </div>
   )
 }
