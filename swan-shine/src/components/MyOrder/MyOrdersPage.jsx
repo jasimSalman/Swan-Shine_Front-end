f22a62305d86a829b88158f73bee6804121be283
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-07-09 15:06:31 +0300

    frontend-my orders page method updated

--- swan-shine/src/components/MyOrder/MyOrdersPage.jsx
+++ swan-shine/src/components/MyOrder/MyOrdersPage.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import OrderCard from './OrderCard'
 import './MyOrdersPage.css'
+import { BASE_URL } from '../../services/api'
 
 const MyOrdersPage = () => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const getShopOrders = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/user/shop/${shopId}/orders`
+        )
+        setOrders(response.data)
+      } catch (err) {
+        console.error('Error fetching orders', err)
+      }
+    }
+    getShopOrders()
+  }, [shopId])
+
   return (
     <div className="my-orders-page">
       <h1>My Orders</h1>
-      <OrderCard />
+      <OrderCard orders={orders} />
     </div>
   )
 }
--- swan-shine/src/components/MyOrder/OrderCard.jsx
+++ swan-shine/src/components/MyOrder/OrderCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 // import './OrderCard.css'
 
-const OrderCard = () => {
+const OrderCard = ({ orders }) => {
   return (
     <div className="order-card">
+      {orders.map((order) => (
+        <div key={order._id} className="order">
+          <img src={shop.poster} alt={shop.name} className="shop-image" />
+          <h2>{order.name}</h2>
+          <p>Category: {shop.category}</p>
+        </div>
+      ))}
+
       <h2>Order</h2>
       <p>Details about the order go here.</p>
     </div>
