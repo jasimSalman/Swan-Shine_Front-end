2a3f880e8c5fec54ba944d38e0a4c9295a2784cd
Jellooking <jassimkhalaf14@gmail.com>
2024-07-09 17:46:31 +0300

    done with myitems folder

--- swan-shine/src/components/MyItems/MyItemsPage.jsx
+++ swan-shine/src/components/MyItems/MyItemsPage.jsx
@@ -1,9 +1,19 @@
+// MyItemsPage.jsx
 import React from 'react'
 import AddItemButton from './AddItemButton'
 import ItemsCard from './ItemsCard'
 import './MyItemsPage.css'
 
 const MyItemsPage = () => {
+  const fakeItems = [
+    { title: 'Item 1', description: 'This is the description for item 1.' },
+    { title: 'Item 2', description: 'This is the description for item 2.' },
+    { title: 'Item 3', description: 'This is the description for item 3.' },
+    { title: 'Item 4', description: 'This is the description for item 4.' },
+    { title: 'Item 5', description: 'This is the description for item 5.' },
+    { title: 'Item 6', description: 'This is the description for item 6.' }
+  ]
+
   return (
     <div className="my-items-page">
       <h1 className="page-title">My Items</h1>
@@ -11,12 +21,13 @@ const MyItemsPage = () => {
         <AddItemButton />
       </div>
       <div className="items-list">
-        <ItemsCard />
-        <ItemsCard />
-        <ItemsCard />
-        <ItemsCard />
-        <ItemsCard />
-        <ItemsCard />
+        {fakeItems.map((item, index) => (
+          <ItemsCard
+            key={index}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
       </div>
     </div>
   )
