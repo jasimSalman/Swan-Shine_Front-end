5b84b0154059a6e32e1908ceb7023956e5154902
jenan <jenanmj15@gmail.com>
2024-07-10 15:09:58 +0300

    conflict resolve

--- swan-shine/src/components/ItemDetails/ItemDetailsPage.jsx
+++ swan-shine/src/components/ItemDetails/ItemDetailsPage.jsx
@@ -26,82 +26,82 @@ const ItemDetailsPage = () => {
       console.error('Error fetching the item details:', error)
       setLoading(false)
     }
-
-    const fetchReviews = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL}/items/${id}/reviews`)
-        setReviews(response.data)
-      } catch (error) {
-        console.error('Error fetching reviews:', error)
-      }
-    }
-
-    useEffect(() => {
-      fetchItemDetails()
-    }, [id])
-
-    const handleAddToCart = async (item) => {
-      try {
-        await Client.post(`${BASE_URL}/cart/${userId}`, {
-          items: [{ item: item, quantity: 1 }]
-        })
-        navigate('/cart')
-      } catch (error) {
-        console.error('Error adding item to cart:', error)
-      }
-    }
-
-    const handleAddReview = (review) => {
-      setItem((prevItem) => ({
-        ...prevItem,
-        reviews: [...prevItem.reviews, review]
-      }))
-      setIsReviewModalOpen(false)
-      fetchReviews()
-    }
-
-    if (loading) {
-      return <div>Loading...</div>
-    }
-
-    if (!item) {
-      return <div>Error loading item details</div>
-    }
-
-    return (
-      <div className="item-details-page">
-        <h1>Item Details</h1>
-        <ItemDetailsCard
-          item={item}
-          onAddToCart={handleAddToCart}
-          onAddReview={() => setIsReviewModalOpen(true)}
-        />
-        <Review
-          isOpen={isReviewModalOpen}
-          onRequestClose={() => setIsReviewModalOpen(false)}
-          itemId={id}
-          onReviewSubmitted={handleAddReview}
-        />
-        <Rating rating={item.rating} />{' '}
-        <div className="reviews-container">
-          <h2>Reviews</h2>
-          {reviews.length === 0 ? (
-            <p>No reviews available.</p>
-          ) : (
-            <ul>
-              {reviews.map((review) => (
-                <li key={review._id}>
-                  <p>{review.content}</p>
-                  <Rating rating={review.rating} />{' '}
-                  <p>User: {review.user.username}</p>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-    )
   }
+
+  const fetchReviews = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/items/${id}/reviews`)
+      setReviews(response.data)
+    } catch (error) {
+      console.error('Error fetching reviews:', error)
+    }
+  }
+
+  useEffect(() => {
+    fetchItemDetails()
+  }, [id])
+
+  const handleAddToCart = async (item) => {
+    try {
+      await Client.post(`${BASE_URL}/cart/${userId}`, {
+        items: [{ item: item, quantity: 1 }]
+      })
+      navigate('/cart')
+    } catch (error) {
+      console.error('Error adding item to cart:', error)
+    }
+  }
+
+  const handleAddReview = (review) => {
+    setItem((prevItem) => ({
+      ...prevItem,
+      reviews: [...prevItem.reviews, review]
+    }))
+    setIsReviewModalOpen(false)
+    fetchReviews()
+  }
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!item) {
+    return <div>Error loading item details</div>
+  }
+
+  return (
+    <div className="item-details-page">
+      <h1>Item Details</h1>
+      <ItemDetailsCard
+        item={item}
+        onAddToCart={handleAddToCart}
+        onAddReview={() => setIsReviewModalOpen(true)}
+      />
+      <Review
+        isOpen={isReviewModalOpen}
+        onRequestClose={() => setIsReviewModalOpen(false)}
+        itemId={id}
+        onReviewSubmitted={handleAddReview}
+      />
+      <Rating rating={item.rating} />{' '}
+      <div className="reviews-container">
+        <h2>Reviews</h2>
+        {reviews.length === 0 ? (
+          <p>No reviews available.</p>
+        ) : (
+          <ul>
+            {reviews.map((review) => (
+              <li key={review._id}>
+                <p>{review.content}</p>
+                <Rating rating={review.rating} />{' '}
+                <p>User: {review.user.username}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default ItemDetailsPage
