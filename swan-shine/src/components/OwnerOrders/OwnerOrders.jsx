06f86a02792f61027e98fa177201226834246d5c
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-07-10 12:18:02 +0300

    created a useEffect with a function that gets all shop orders to owner

---
+++ swan-shine/src/components/OwnerOrders/OwnerOrders.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import OrderCard from './OrderCard'
+import './OwnerOrders.css'
+import { BASE_URL } from '../../services/api'
+import axios from 'axios'
+
+const OwnerOrdersPage = () => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const getShopOrders = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/users/shop/:shopId/orders`
+        )
+        setOrders(response.data)
+      } catch (err) {
+        console.error('Error fetching orders', err)
+      }
+    }
+    getShopOrders()
+  }, [shopId])
+
+  return ()
+}
+
+export default OwnerOrdersPage
