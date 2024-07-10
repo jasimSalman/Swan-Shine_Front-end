39d9f20b8ea62732fec97fadb8e528a488130968
Sara Alalawi <sarah.alalawi92@gmail.com>
2024-07-10 17:55:35 +0300

    user orders fixed

--- swan-shine/src/components/MyOrder/OrderCard.jsx
+++ swan-shine/src/components/MyOrder/OrderCard.jsx
@@ -4,26 +4,22 @@ import './MyOrdersPage.css'
 const OrderCard = ({ order }) => {
   return (
     <div className="order-card">
-      {orders.map((order) => (
-        <div key={order._id} className="order">
-          <img
-            src={order.items[0].item.image}
-            alt={order.items[0].item.name}
-            className="item-image"
-          />
-          <h2>{order.user.username}</h2>
-          <p>Total Price: {order.total_price}</p>
-          <ul>
-            {order.items.map((item) => (
-              <li key={item.item._id}>
-                <p>{item.item.name}</p>
-                <p>Quantity:{item.quantity}</p>
-                <p>{item.date}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <h2>{order.user.username}</h2>
+      <p>Total Price: {order.total_price}</p>
+      <ul>
+        {order.items.map((item) => (
+          <li key={item.item._id}>
+            <p>{item.item.name}</p>
+            <p>Quantity: {item.quantity}</p>
+            <p>{item.date}</p>
+            <img
+              src={item.item.image}
+              alt={item.item.name}
+              className="item-image"
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
